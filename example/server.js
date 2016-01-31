77975793f728f53dcc6bae96f6aecf2e29fcd6ca
pacozaa <pacosarin@gmail.com>
2016-01-31 19:44:08 +0700

    add important usage
    add important usage

--- example/server.js
+++ example/server.js
@@ -12,12 +12,12 @@ server.listen(6147, function(){
 	console.log('Listening on localhost:6147');
 });
 
-
+//udp://localhost:1234/1234 need an additional path or the lib can not ge tthe streaming
 var cams = [
 		'rtsp://r3---sn-5hn7su76.c.youtube.com/CiILENy73wIaGQnup-1SztVOYBMYESARFEgGUgZ2aWRlb3MM/0/0/0/video.3gp'
 		, 'rtsp://192.168.68.111/h264main'
 		, 'rtsp://r1---sn-5hn7su76.c.youtube.com/CiILENy73wIaGQn8uA5p5adowhMYDSANFEgGUgZ2aWRlb3MM/0/0/0/video.3gp'
-		, 'udp://localhost:1234'
+		, 'udp://localhost:1234/1234'
 	].map(function(uri, i) {
 		var stream = new rtsp.FFMpeg({input: uri, resolution: '320x240', quality: 3});
 		stream.on('start', function() {
